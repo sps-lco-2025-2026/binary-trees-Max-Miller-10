fe65772c82b9da8bf0be397a41c529b9327280fb
Max-Miller-10 <millermg@stpaulsschool.org.uk>
2026-03-14 13:52:56 +0000

    Completed upto Medium Difficulty
    I skipped sum method because I wrote it as a generic class and did not with to force the type T to implement addition (because it is not needed for a binary search tree).

---
+++ BinaryTree/BinaryTree.Lib/BinaryTree.cs
@@ -0,0 +1,57 @@
+﻿using System.Numerics;
+
+namespace BinaryTree.Lib
+{
+
+    public class BinaryTree<T> where T: IComparable<T>
+    {
+        Node<T>? root;
+
+        public BinaryTree()
+        {
+        }
+
+        public BinaryTree(T val)
+        {
+           root = new Node<T>(val);
+        }
+
+        public void Insert(T val)
+        {
+            if (root == null) root = new Node<T>(val);
+            else root.Insert(val);
+        }
+
+        public override string ToString()
+        {
+            return root?.ToString() ?? "";
+        }
+
+        public bool Contains (T val)
+        {
+            return root?.Contains(val) ?? false;
+        }
+
+        public bool Duplicates()
+        {
+            return root?.Duplicates() ?? false;
+        }
+
+        public int Depth
+        {
+            get
+            {
+                return root?.Depth ?? 0;
+            }
+        }
+
+        public bool IsBalanced
+        {
+            get
+            {
+                return root?.IsBalanced ?? true;
+            }
+        }
+
+    }
+}
---
+++ BinaryTree/BinaryTree.Test/Tests.cs
@@ -0,0 +1,83 @@
+﻿using BinaryTree.Lib;
+namespace BinaryTree.Test
+{
+
+
+    [TestClass]
+    public sealed class TestBinaryTree
+    {
+        [TestMethod]
+        public void TestInsert()
+        {
+            BinaryTree<int> bt = new(7);
+            bt.Insert(4);
+            bt.Insert(18);
+            bt.Insert(20);
+            bt.Insert(19);
+            bt.Insert(8);
+            bt.Insert(5);
+            Assert.AreEqual("4, 5, 7, 8, 18, 19, 20", bt.ToString());
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            BinaryTree<int> bt = new(7);
+            bt.Insert(4);
+            bt.Insert(18);
+            bt.Insert(20);
+            bt.Insert(19);
+            bt.Insert(8);
+            bt.Insert(5);
+            Assert.IsTrue(bt.Contains(18));
+            Assert.IsTrue(bt.Contains(19));
+            Assert.IsTrue(bt.Contains(5));
+            Assert.IsFalse(bt.Contains(6));
+        }
+
+        [TestMethod]
+        public void TestDuplicates()
+        {
+            BinaryTree<int> bt = new(7);
+            bt.Insert(4);
+            bt.Insert(18);
+            bt.Insert(20);
+            bt.Insert(19);
+            bt.Insert(8);
+            bt.Insert(5);
+            Assert.IsFalse (bt.Duplicates());
+            bt.Insert(19);
+            Assert.IsTrue (bt.Duplicates());
+        }
+
+        [TestMethod] 
+        public void TestDepth()
+        {
+            BinaryTree<int> bt = new(7);
+            bt.Insert(4);
+            bt.Insert(18);
+            bt.Insert(20);
+            bt.Insert(19);
+            bt.Insert(8);
+            bt.Insert(5);
+            Assert.AreEqual(4, bt.Depth);
+        }
+
+        [TestMethod]
+        public void TestBalanced()
+        {
+            BinaryTree<int> bt = new(4);
+            bt.Insert(2);
+            bt.Insert(5);
+            bt.Insert(1);
+            bt.Insert(3);
+            bt.Insert(6);
+            Assert.IsTrue(bt.IsBalanced);
+            bt = new(1);
+            bt.Insert(2);
+            bt.Insert(5);
+            bt.Insert(6);
+            Assert.IsFalse(bt.IsBalanced);
+        }
+    }
+}
